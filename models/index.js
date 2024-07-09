5f2d4804c71835a1e6d69edf1d497cebc0767971
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-07-09 12:46:08 -0700

    added comments

--- models/index.js
+++ models/index.js
@@ -1,13 +1,27 @@
 const User = require('./User');
 const Blog = require('./Blog');
+const Comment = require('./Comment');
 
+// User has many Blogs
 User.hasMany(Blog, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
+// Blog belongs to User
 Blog.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Blog };
+// Blog has many Comments
+Blog.hasMany(Comment, {
+  foreignKey: 'blog_id',
+  onDelete: 'CASCADE'
+});
+
+// Comment belongs to Blog
+Comment.belongsTo(Blog, {
+  foreignKey: 'blog_id'
+});
+
+module.exports = { User, Blog, Comment };
