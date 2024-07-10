c39e867194f4ad03be62b7c4b392bbc857d080f8
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-07-09 21:33:38 -0700

    added profileRoutes

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const profileRoutes = require('./profileRoutes');
 
 router.use('/', homeRoutes); // homeRoutes return HTML
 router.use('/api', apiRoutes); // apiRoutes return JSON
+router.use('/profile', profileRoutes); // profileRoutes handle profile-related routes
 
 module.exports = router;
