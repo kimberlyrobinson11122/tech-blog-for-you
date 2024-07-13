3e571392a533265d1670bbb66d1ce0f4b696788f
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-07-12 19:01:55 -0700

    added user_id to get the comment id

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -4,16 +4,21 @@ const withAuth = require('../../utils/auth');
 
 // POST new comment
 router.post('/', withAuth, async (req, res) => {
+    console.log('Hitting the new comment route');
     try {
-        const { user_id, comment_text, blog_id } = req.body; // Destructure the required fields from req.body
+        const { comment_text, blog_id } = req.body;
+        const user_id = req.session.user_id;
 
         const newComment = await Comment.create({
             user_id,
             comment_text,
             blog_id,
+            comment_author: req.session.name
         });
 
-        res.status(200).json(newComment);
+        console.log('New comment created:', newComment);
+
+        res.status(201).json(newComment); // Use status 201 for successful creation
     } catch (err) {
         console.error('Error creating comment:', err.message);
         res.status(400).json({ error: 'Failed to create comment' });
@@ -22,6 +27,7 @@ router.post('/', withAuth, async (req, res) => {
 
 // DELETE comment
 router.delete('/:id', withAuth, async (req, res) => {
+    console.log(id)
     try {
         const commentData = await Comment.findByPk(req.params.id);
         if (!commentData) {
@@ -33,10 +39,9 @@ router.delete('/:id', withAuth, async (req, res) => {
         await commentData.destroy();
         res.status(200).json({ message: 'Comment deleted' });
     } catch (err) {
+        console.error('Error deleting comment:', err.message);
         res.status(500).json({ error: 'Internal server error' });
     }
 });
-
-
 
 module.exports = router;
