4803ec1070445188330301d01d7434e5757d8e92
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-07-13 17:01:21 -0700

    updated

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const blogRoutes = require('./blogRoutes'); 
-const commentRoutes = require('./commentRoutes'); 
+const blogRoutes = require('./blogRoutes');
+const commentRoutes = require('./commentRoutes');
 const userRoutes = require('./userRoutes');
 
 router.use('/users', userRoutes);
-router.use('/blogs', blogRoutes); 
+router.use('/blogs', blogRoutes);
 router.use('/comments', commentRoutes);
 
 module.exports = router;
